9f49fbe62d4880674c68df399b8aeac1e0fdf2b6
Srinithi Ravichandran <emailtonithi@gmail.com>
2023-03-01 10:22:50 -0800

    implememnt getdb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -23,7 +23,12 @@ export const putDb = async (content) => {
 }
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.get(1);
+  const result = await request;
+  console.log("🚀 - data saved to the database", result);
 }
 
 initdb();
